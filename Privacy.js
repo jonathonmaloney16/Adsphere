41a2475991212b495fc155d97a7b94544f1bac56
jonathon maloney <jonathonmaloney16@gmail.com>
2025-09-08 17:39:16 -0700

    Delete Privacy.js

--- Privacy.js
+++
@@ -1,8 +0,0 @@
-export default function Privacy() {
-  return (
-    <div className="container">
-      <h1 className="h1">Privacy Policy</h1>
-      <div className="card">Template placeholder. Update for your data flows.</div>
-    </div>
-  );
-}
