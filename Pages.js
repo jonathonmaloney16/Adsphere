073dabfe2c638be72723d1927e38cdde67520f5b
jonathon maloney <jonathonmaloney16@gmail.com>
2025-09-08 18:46:14 -0700

    Delete Pages.js

--- Pages.js
+++
@@ -1,9 +0,0 @@
-export default function FourOhFour() {
-  return (
-    <div className="container">
-      <h1 className="h1">404</h1>
-      <p className="muted">Page not found.</p>
-      <a className="btn" href="/">← Back home</a>
-    </div>
-  );
-}
